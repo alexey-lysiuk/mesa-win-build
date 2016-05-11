021d11cd66ca22ff227b6821f980ca8379f323dc
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-05-11 14:45:14 +0300

    + Setuptools 21.0.0
    https://pypi.python.org/pypi/setuptools
    https://pypi.python.org/packages/1d/c0/57c129132059297460922eec3cabf3016e792455adf9db8c5b144db67d69/setuptools-21.0.0.zip#md5=1da14a897449567b4e151d7461a0c829

---
+++ tools/scripts/easy_install-2.7-script.py
@@ -0,0 +1,10 @@
+#!e:\Temp\mesa-win-build\tools\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==21.0.0','console_scripts','easy_install-2.7'
+__requires__ = 'setuptools==21.0.0'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('setuptools==21.0.0', 'console_scripts', 'easy_install-2.7')()
+    )
